07304fc399a6889805a0990c89a4f73a10ca00d0
tjacek <tjacek@student.agh.edu.pl>
2019-08-11 23:40:03 +0200

    read data_dict

--- data.py
+++ data.py
@@ -2,6 +2,18 @@ import cv2,numpy as np
 import re,os
 import files
 
+def dict_dataset(in_path):
+    data_dict={}
+    action_dirs=files.top_files(in_path)
+    for path_i in action_dirs:
+        frame_paths=files.top_files(path_i)            
+        seq_i=[]
+        for frame_ij_path in frame_paths:
+            seq_i.append(read_frame(frame_ij_path))
+        name_i=path_i.split('/')
+        data_dict[name_i]=seq_i  
+    return data_dict
+    
 def frame_dataset(in_path):
     action_dirs=files.top_files(in_path)
     train,test=split(action_dirs)
