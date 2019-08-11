413eabbe68adcdad7a83c55eb5d383f722be0440
tjacek <tjacek@student.agh.edu.pl>
2019-08-11 20:48:16 +0200

    residual network

--- resnet.py
+++ resnet.py
@@ -6,6 +6,7 @@ from keras.layers import Conv2D, MaxPooling2D
 from sklearn.metrics import classification_report
 from keras import regularizers
 from keras.layers.normalization import BatchNormalization
+from keras.layers.merge import add
 import data,norm
 
 def train_conv(in_path,n_epochs=100):
@@ -14,7 +15,7 @@ def train_conv(in_path,n_epochs=100):
             'n_cats': max(test_y)+1}
     (train_X,train_y),(test_X,test_y)= prepare_data(train_X,train_y),prepare_data(test_X,test_y)
     print(params)
-    model=make_conv(params)
+    model=make_res(params)
     model.fit(train_X,train_y,epochs=n_epochs,batch_size=32)
     raw_pred=model.predict(test_X,batch_size=32)
     pred_y,test_y=np.argmax(raw_pred,axis=1),np.argmax(test_y,axis=1)
@@ -28,12 +29,30 @@ def prepare_data(X,y):
     y=keras.utils.to_categorical(y)
     return X,y
 
+def make_res(params):
+    input_layer = Input(shape=(params['ts_len'], params['n_feats'],1))
+    activ='relu' #'elu'
+    pool1=add_conv_layer(input_layer,activ=activ)
+    pool2=add_conv_layer(pool1,activ=activ)
+
+    hidden_layer = Dense(64, activation=activ)(Flatten()(pool2))   
+    hidden_layer2=Dense(64, activation=activ)(hidden_layer)
+    res_layer=add([hidden_layer, hidden_layer2])
+    drop1=Dropout(0.5)(res_layer)
+    output_layer = Dense(units=params['n_cats'], activation='softmax')(drop1)
+    model=Model(inputs=input_layer, outputs=output_layer)
+    model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.SGD(lr=0.01,  momentum=0.9, nesterov=True))
+    model.summary()
+    return model
+
 def make_conv(params):
     input_layer = Input(shape=(params['ts_len'], params['n_feats'],1))
-    pool1=add_conv_layer(input_layer)
-    pool2=add_conv_layer(pool1)
+    activ='relu' #'elu'
+    pool1=add_conv_layer(input_layer,activ=activ)
+    pool2=add_conv_layer(pool1,activ=activ)
 
-    hidden_layer = Dense(100, activation='relu')(Flatten()(pool2))
+    hidden_layer = Dense(100, activation=activ)(Flatten()(pool2))
     #kernel_regularizer=regularizers.l1(0.01)
     hidden_layer=BatchNormalization()(hidden_layer)
     drop1=Dropout(0.5)(hidden_layer)
@@ -44,9 +63,10 @@ def make_conv(params):
     model.summary()
     return model
 
-def add_conv_layer(input_layer,n_kerns=8,kern_size=(8,1),pool_size=(4,1)):
+def add_conv_layer(input_layer,n_kerns=8,activ='relu',
+                    kern_size=(8,1),pool_size=(4,1)):
     conv1=Conv2D(n_kerns, kernel_size=kern_size,
-            activation='relu')(input_layer)
+            activation=activ)(input_layer)
     pool1=MaxPooling2D(pool_size=pool_size)(conv1)
     return BatchNormalization()(pool1)
 
