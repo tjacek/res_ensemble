dd6c6e870aa5244d81cec4c750bcd4d38913a673
tjacek <tjacek@student.agh.edu.pl>
2019-08-12 17:06:42 +0200

    extract features

--- extract.py
+++ extract.py
@@ -3,23 +3,28 @@ from keras.models import Model
 from keras.models import load_model
 import data,files
 
-def extract_features(frame_path,model_path):
+def extract_features(frame_path,model_path,out_path):
     seq_dict=data.dict_dataset(frame_path)
-    models=read_models(model_path)
-    extractors=[make_extractor(model_i) for model_i in models]
-    frame_features(seq_dict,extractors[0])
+    frame_feats=[]
+    for path_i in files.top_files(model_path):
+        print(path_i)
+        model_i=load_model(path_i)
+        extractor_i=make_extractor(model_i)
+        frame_feats.append(frame_features(seq_dict,extractor_i))
+    files.make_dir(out_path)
+    for i,feats_i in enumerate(frame_feats):
+        out_i=out_path+'/nn'+str(i)
+        files.make_dir(out_i)
+        for name_j,seq_j in feats_i.items():
+            out_ij=out_i+'/'+name_j
+            np.savetxt(out_ij,seq_j, delimiter='')
+    return frame_feats
 
 def frame_features(data_dict,extractor):
     new_dict={}
     for name_i,seq_i in data_dict.items():
         new_dict[name_i]=extractor.predict(np.array(seq_i))
-#        np.array([extractor.predict(frame_ij) for frame_ij in seq_i])
-        print(new_dict[name_i].shape)
     return new_dict
-
-
-def read_models(in_path):
-    return [load_model(path_i) for path_i in files.top_files(in_path)]
 
 def make_extractor(model):
     return Model(inputs=model.input,
