eca57bb037df3f953feaffea7ca91676f159ea1e
tjacek <tjacek@student.agh.edu.pl>
2019-08-12 18:55:08 +0200

    read seqs

--- extract.py
+++ extract.py
@@ -11,13 +11,7 @@ def extract_features(frame_path,model_path,out_path):
         model_i=load_model(path_i)
         extractor_i=make_extractor(model_i)
         frame_feats.append(frame_features(seq_dict,extractor_i))
-    files.make_dir(out_path)
-    for i,feats_i in enumerate(frame_feats):
-        out_i=out_path+'/nn'+str(i)
-        files.make_dir(out_i)
-        for name_j,seq_j in feats_i.items():
-            out_ij=out_i+'/'+name_j
-            np.savetxt(out_ij,seq_j, delimiter='')
+    save_seqs(frame_feats,out_path)
     return frame_feats
 
 def frame_features(data_dict,extractor):
@@ -29,3 +23,21 @@ def frame_features(data_dict,extractor):
 def make_extractor(model):
     return Model(inputs=model.input,
                 outputs=model.get_layer("hidden").output)
+
+def save_seqs(frame_feats,out_path):
+    files.make_dir(out_path)
+    for i,feats_i in enumerate(frame_feats):
+        out_i=out_path+'/nn'+str(i)
+        files.make_dir(out_i)
+        for name_j,seq_j in feats_i.items():
+            out_ij=out_i+'/'+name_j
+            np.savetxt(out_ij,seq_j, delimiter=',')
+
+def read_seqs(in_path):
+    return [ read_dict(path_i)
+                for path_i in files.top_files(in_path)]
+        
+def read_dict(dict_path):
+    return { path_i.split('/')[-1]:np.loadtxt(path_i,delimiter=',') 
+                for path_i in files.top_files(dict_path)}
+
