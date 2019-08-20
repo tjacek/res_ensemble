025f0998ebb823490f554faef17e151e31fd3beb
tjacek <tjacek@student.agh.edu.pl>
2019-08-20 16:26:18 +0200

    pretrained model

--- resnet.py
+++ resnet.py
@@ -30,15 +30,36 @@ def train_conv(in_path,out_path=None,n_epochs=100):
     if(out_path):
         model.save(out_path)
 
+def pretrained_model(in_path,nn_path,n_epochs=100):
+    (train_X,train_y),(test_X,test_y),params=load_data(in_path)
+    pretrain_model=load_model(nn_path)
+    pretrain_model.summary()
+    hidden=pretrain_model.get_layer("hidden").output
+    drop=Dropout(0.5)(hidden)
+    output=Dense(units=params['n_cats'], activation='softmax')(drop) 
+    model=Model(inputs=pretrain_model.input, outputs=output)
+    model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.SGD(lr=0.01,  momentum=0.9, nesterov=True),
+              metrics=['accuracy'])
+    model.summary()
+    model.fit(train_X,train_y,epochs=n_epochs,batch_size=32)
+    score = model.evaluate(test_X,test_y, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+
 def train_model(in_path,n_epochs):
-    (train_X,train_y),(test_X,test_y)=data.img_dataset(in_path)
-    params={'ts_len':train_X[0].shape[0],'n_feats':train_X[0].shape[1],
-            'n_cats': max(test_y)+1}
-    (train_X,train_y),(test_X,test_y)= prepare_data(train_X,train_y),prepare_data(test_X,test_y)
+    (train_X,train_y),(test_X,test_y),params=load_data(in_path)
     print(params)
-    model=make_res(params)
+    model=make_conv(params)
     model.fit(train_X,train_y,epochs=n_epochs,batch_size=32)
     return model,(test_X,test_y)
+
+def load_data(in_path):
+    (train_X,train_y,names),(test_X,test_y,names)=data.img_dataset(in_path)
+    params={'ts_len':train_X[0].shape[0],'n_feats':train_X[0].shape[1],
+            'n_cats': max(test_y)+1}
+    return prepare_data(train_X,train_y),prepare_data(test_X,test_y),params
 
 def prepare_data(X,y):
     X=norm.normalize(X,'all')
@@ -52,8 +73,8 @@ def make_res(params):
     pool1=add_conv_layer(input_layer,activ=activ)
     pool2=add_conv_layer(pool1,activ=activ)
 
-    res_layer1=add_res_layer(Flatten()(pool2),n_hidden=100,activ=activ)
-    res_layer2=add_res_layer(res_layer1,n_hidden=100,activ=activ,name="hidden")
+    #res_layer1=add_res_layer(Flatten()(pool2),n_hidden=100,activ=activ,l1=True)
+    res_layer2=add_res_layer(Flatten()(pool2),n_hidden=64,activ=activ,l1=True,name="hidden")
     
     drop1=Dropout(0.5)(res_layer2)
     output_layer = Dense(units=params['n_cats'], activation='softmax')(drop1)
@@ -66,26 +87,27 @@ def make_res(params):
 def make_conv(params):
     input_layer = Input(shape=(params['ts_len'], params['n_feats'],1))
     activ='relu' #'elu'
-    pool1=add_conv_layer(input_layer,activ=activ)
-    pool2=add_conv_layer(pool1,activ=activ)
+    pool1=add_conv_layer(input_layer,0,activ=activ)
+    pool2=add_conv_layer(pool1,1,activ=activ)
 
-    hidden_layer = Dense(100, activation=activ)(Flatten()(pool2))
+    hidden_layer = Dense(100,name='hidden', activation=activ)(Flatten()(pool2))
     #kernel_regularizer=regularizers.l1(0.01)
-    hidden_layer=BatchNormalization()(hidden_layer)
+    #hidden_layer=BatchNormalization()(hidden_layer)
     drop1=Dropout(0.5)(hidden_layer)
-    output_layer = Dense(units=params['n_cats'], activation='softmax',name="hidden")(drop1)
+    output_layer = Dense(units=params['n_cats'], activation='softmax')(drop1)
     model=Model(inputs=input_layer, outputs=output_layer)
     model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.SGD(lr=0.01,  momentum=0.9, nesterov=True))
     model.summary()
     return model
 
-def add_conv_layer(input_layer,n_kerns=8,activ='relu',
+def add_conv_layer(input_layer,i=0,n_kerns=8,activ='relu',
                     kern_size=(8,1),pool_size=(4,1)):
+    i=str(i)
     conv1=Conv2D(n_kerns, kernel_size=kern_size,
-            activation=activ)(input_layer)
-    pool1=MaxPooling2D(pool_size=pool_size)(conv1)
-    return BatchNormalization()(pool1)
+            activation=activ,name='conv'+i)(input_layer)
+    pool1=MaxPooling2D(pool_size=pool_size,name='pool'+i)(conv1)
+    return pool1#BatchNormalization()(pool1)
 
 def add_res_layer(input_layer,n_hidden=64,activ='relu',l1=False,name=None):
     ker_reg=regularizers.l1(0.01) if(l1) else None
