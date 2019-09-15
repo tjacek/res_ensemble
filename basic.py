c444f0e5316466f258dcece9cbcc520eaad8f548
tjacek <tjacek@agh.edu.pl>
2019-09-15 21:30:41 +0200

    basic experiment

--- basic.py
+++ basic.py
@@ -5,27 +5,59 @@ from keras.layers import Input,Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.merge import add
 from keras import regularizers
-from keras.layers.normalization import BatchNormalization
-import data,files
+from sklearn.metrics import classification_report
+#from keras.layers.normalization import BatchNormalization
+import data#,files
 
-def train_binary_model(in_path,out_path,n_epochs=1500):
-    train_X,train_y=data.frame_dataset(in_path)
-    train_y=keras.utils.to_categorical(train_y)
-    files.make_dir(out_path)
-    n_cats=train_y.shape[1]
-    for cat_i in range(n_cats):
-        y_i=binarize(train_y,cat_i)
-        model=make_res(2)
-        model.summary()
-        model.fit(train_X,y_i,epochs=n_epochs,batch_size=256)
-        out_i=out_path+'/nn'+str(cat_i)
-        model.save(out_i)
 
-def make_conv(n_cats):
+def simple_exp(in_path,n_epochs=10):
+    (X_train,y_train),(X_test,y_test)=data.make_dataset(in_path)
+    n_cats=count_cats(y_train)
+    n_channels=count_channels(X_train) 
+    X_train,y_train=prepare_data(X_train,y_train,n_channels)
+    X_test,y_test=prepare_data(X_test,y_test,n_channels)
+
+    model=make_conv(n_cats,n_channels)
+    model.fit(X_train,y_train,epochs=n_epochs,batch_size=256)
+
+    raw_pred=model.predict(X_test,batch_size=256)
+    pred_y,test_y=np.argmax(raw_pred,axis=1),np.argmax(y_test,axis=1)
+    print(classification_report(test_y, pred_y,digits=4))
+
+def prepare_data(X,y,n_channels):
+    X=format_frames(X,n_channels)
+    y=keras.utils.to_categorical(y)
+    return X,y
+
+def count_cats(y):
+    return np.unique(np.array(y)).shape[0]
+
+def count_channels(X):
+    frame_dims=X[0].shape
+    return int(frame_dims[0]/frame_dims[1])
+
+def format_frames(frames ,n_channels):
+    return np.array([np.array(np.vsplit(frame_i,n_channels)).T
+                      for frame_i in frames])
+
+#def train_binary_model(in_path,out_path,n_epochs=1500):
+#    train_X,train_y=data.frame_dataset(in_path)
+#    train_y=keras.utils.to_categorical(train_y)
+#    files.make_dir(out_path)
+#    n_cats=train_y.shape[1]
+#    for cat_i in range(n_cats):
+#        y_i=binarize(train_y,cat_i)
+#        model=make_res(2)
+#        model.summary()
+#        model.fit(train_X,y_i,epochs=n_epochs,batch_size=256)
+#        out_i=out_path+'/nn'+str(cat_i)
+#        model.save(out_i)
+
+def make_conv(n_cats,n_channels):
     model = Sequential()
     model.add(Conv2D(16, kernel_size=(5, 5),
                  activation='relu',
-                 input_shape=(64,64,4)))
+                 input_shape=(64,64,n_channels)))
     model.add(MaxPooling2D(pool_size=(4, 4)))
     model.add(Conv2D(16, kernel_size=(5, 5),
                  activation='relu',input_shape=(64,64,4)))
@@ -77,9 +109,9 @@ def make_res(n_cats):
     model.summary()
     return model
 
-def binarize(train_y,cat_i):
-    y=np.zeros((train_y.shape[0],2))
-    for i,one_hot_i in enumerate(train_y):
-      j=int(one_hot_i[cat_i]==1)
-      y[i][j]=1 
-    return y
+#def binarize(train_y,cat_i):
+#    y=np.zeros((train_y.shape[0],2))
+#    for i,one_hot_i in enumerate(train_y):
+#      j=int(one_hot_i[cat_i]==1)
+#      y[i][j]=1 
+#    return y
--- data.py
+++ data.py
@@ -1,64 +1,33 @@
-import cv2,numpy as np
+#import cv2,numpy as np
 import re,os
-import files
+import imgs
 
-def dict_dataset(in_path):
-    data_dict={}
-    action_dirs=files.top_files(in_path)
-    for path_i in action_dirs:
-        frame_paths=files.top_files(path_i)            
-        seq_i=[]
-        for frame_ij_path in frame_paths:
-            seq_i.append(read_frame(frame_ij_path))
-        name_i=path_i.split('/')[-1]
-        data_dict[name_i]=seq_i  
-    return data_dict
-    
-def frame_dataset(in_path):
-    action_dirs=files.top_files(in_path)
-    train,test=split(action_dirs)
-    return read_frames(train)
+def make_dataset(in_path):
+    img_seqs=imgs.read_seqs(in_path)
+    train,test=split(img_seqs.keys())
+    return to_dataset(train,img_seqs),to_dataset(test,img_seqs)
 
-def img_dataset(in_path,split_data=True):
-    action_dirs=files.top_files(in_path)
-    if(split_data):
-        train,test=split(action_dirs)
-        return read_imgs(train),read_imgs(test)
-    return read_imgs(action_dirs)
-
-def read_frames(actions):
-    X,y=[],[]
-    for action_i in actions:
-        cat_i,person_i= parse_name(action_i)
-        frame_path=files.top_files(action_i)            
-        for frame_ij_path in frame_path:
-            X.append(read_frame(frame_ij_path))                
-            y.append( cat_i-1)
-    return np.array(X),y
-
-def read_imgs(actions):
-    X,y,names=[],[],[]
-    for action_i in actions:
-        cat_i,person_i= parse_name(action_i)
-        X.append(cv2.imread(action_i,0).astype(float))
-        y.append(cat_i-1)
-        names.append(action_i.split('/')[-1])
-    return np.array(X),y,names
-
-def split(names):
+def split(names,selector=None):
+    if(not selector):
+        selector=lambda name_i: (parse_name(name_i)[1]%2==1)
     train,test=[],[]
     for name_i in names:
         if((parse_name(name_i)[1]%2)==1):
             train.append(name_i)
         else:
             test.append(name_i)
-    return train,test
+    return train,test    
 
 def parse_name(action_i):
     name_i=action_i.split('/')[-1]
     digits=re.findall(r'\d+',name_i)
     return int(digits[0]),int(digits[1])
 
-def read_frame(frame_ij_path,n_split=4):
-    frame_ij=cv2.imread(frame_ij_path,0)
-    return np.array(np.vsplit(frame_ij,n_split)).T
+def to_dataset(names,img_seq):
+    X,y=[],[]
+    for name_i in names:
+        cat_i=parse_name(name_i)[0]-1
+        for frame_j in img_seq[name_i]:
+            X.append(frame_j)
+            y.append(cat_i)
+    return X,y
