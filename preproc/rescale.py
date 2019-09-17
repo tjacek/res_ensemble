18496ed6b0a12613efac7556648c137182f761d3
tjacek <tjacek@agh.edu.pl>
2019-09-17 14:25:49 +0200

    prejections

--- preproc/rescale.py
+++ preproc/rescale.py
@@ -16,8 +16,8 @@ def pairs(in_path,out_path):
 
 def projection(in_path,out_path,dim_x=64,dim_y=64):
     def proj_helper(frame_i):
-        proj_xz= cv2.resize(proj(frame_i,dim=True),(dim_x,dim_y), interpolation = cv2.INTER_CUBIC)
-        proj_yz= cv2.resize(proj(frame_i,dim=False),(dim_x,dim_y), interpolation = cv2.INTER_CUBIC)
+        proj_xz= smooth_proj(proj(frame_i,dim=True),dim_x,dim_y)
+        proj_yz= smooth_proj(proj(frame_i,dim=False),dim_x,dim_y)
         return np.concatenate([proj_xz,proj_yz],axis=0)
     imgs.transform(in_path,out_path,proj_helper,True)
 
@@ -29,7 +29,7 @@ def proj(frame_i,dim=True):
     proj_i=np.zeros((max_x+5,max_y+5))
     for point_j in points:
         x_j,y_j=int(point_j[dim]),int(point_j[2])
-        proj_i[x_j][y_j]=200.0
+        proj_i[x_j][y_j]=1.0
     return proj_i
 
 def to_points(frame_i):
@@ -39,3 +39,19 @@ def to_points(frame_i):
         point_i=np.array([x_i,y_i,frame_i[x_i][y_i]  ])
         points.append(point_i)
     return np.array(points)
+
+def smooth_proj(proj_i,dim_x,dim_y):
+    binary_img=remove_isol(proj_i)
+    kernel2= np.ones((7,7),np.uint8)
+    binary_img = cv2.dilate(binary_img,kernel2,iterations = 1)
+    binary_img[binary_img!=0]=200.0
+    return cv2.resize(binary_img,(dim_x,dim_y), interpolation = cv2.INTER_CUBIC)
+
+def remove_isol(img_i):
+    kernel = np.ones((3,3),np.float32)
+    kernel[1][1]=0.0
+    img_i=img_i.astype(float)
+    img_i[img_i!=0]=1.0
+    img_i = cv2.filter2D(img_i,-1,kernel)
+    img_i[ img_i<2.0]=0.0
+    return img_i#binary_img
