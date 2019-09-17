306fd3f509b58147e5a6c585c5524d1c091c81a0
tjacek <tjacek@agh.edu.pl>
2019-09-17 18:47:45 +0200

    exbinary seqs

--- ens.py
+++ ens.py
@@ -1,7 +1,5 @@
-#from keras.models import Model,Sequential
-#from keras.models import load_model
 import numpy as np
-import data,basic,files,models#,data,resnet,feats
+import data,basic,files,models,extract
 
 def train_binary_model(in_path,out_path,n_epochs=1500):
     (X_train,y_train),(X_test,y_test)=data.make_dataset(in_path)
@@ -17,6 +15,13 @@ def train_binary_model(in_path,out_path,n_epochs=1500):
         out_i=out_path+'/nn'+str(cat_i)
         model.save(out_i)
 
+def binary_extract(frame_path,model_path,seq_path):
+    files.make_dir(seq_path)
+    for i,in_i in enumerate(files.top_files(model_path)):
+        print(i)
+        out_i= seq_path+'/'+in_i.split('/')[-1]
+        extract.extract_features(frame_path,in_i,out_i)
+
 def binarize(train_y,cat_i):
     binary_y=[]
     for sample_j in train_y:
@@ -27,36 +32,3 @@ def binarize(train_y,cat_i):
             new_sample_j[1]=1
         binary_y.append(new_sample_j)
     return np.array(binary_y)
-
-#def binary(in_path,out_path,n_epochs=500):
-#    (train_X,train_y),(test_X,test_y),params=resnet.load_data(in_path)
-#    files.make_dir(out_path)
-#    n_cats=train_y.shape[1]
-#    for i in range(n_cats):
-#        params['n_cats']=2
-#        model=resnet.make_conv(params)
-#        y_i= basic.binarize(train_y,i)
-#        model.fit(train_X,y_i,epochs=n_epochs,batch_size=100)
-#        out_i=out_path+'/nn'+str(i)
-#        model.save(out_i)
-
-#def binary_extract(in_path,frame_path,out_path,cat_feats=False):
-#    model_paths=files.top_files(in_path)
-#    models=[load_model(path_i) for path_i in model_paths]
-#    if(cat_feats):
-#        extractors=models	
-#    else:
-#        extractors=[get_extractor(model_i) for model_i in models]
-#    X,y,names=data.img_dataset(frame_path,split_data=False)
-#    X,y=resnet.prepare_data(X,y)
-#    files.make_dir(out_path)
-#    for i,extr_i in enumerate(extractors):
-#        X_feats=extr_i.predict(X)
-#        feat_dict={ names[i]:feat_i for i,feat_i in enumerate(X_feats)}
-#        out_i=out_path+'/'+model_paths[i].split('/')[-1]
-#        print(out_i)
-#        feats.save_feats(feat_dict,out_i)
-
-#def get_extractor(model):
-#    return Model(inputs=model.input,
-#                outputs=model.get_layer("hidden").output)
