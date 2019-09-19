5274d520e1e0097e99849a9a1d7b41e8154a481f
tjacek <tjacek@agh.edu.pl>
2019-09-19 19:55:26 +0200

    add agum to utils

--- preproc/agum.py
+++ preproc/agum.py
@@ -5,7 +5,7 @@ def agum_data(raw_path,agum_path):
     raw_data=imgs.read_seqs(raw_path)
     train,test=data.split(raw_data.keys())
     train_data={ name_i:raw_data[name_i] for name_i in train}
-    agum = iaa.Sequential([iaa.Crop(px=(0, 32))])
+    agum = iaa.Sequential([iaa.Affine(scale=(1.0, 1.5))])#[iaa.Crop(px=(0, 32))])
     agum_dict={}
     for name_i,seq_i in list(train_data.items()):
         agum_seq_i = agum(images=seq_i)
--- utils.py
+++ utils.py
@@ -1,9 +1,18 @@
 import os.path
 import basic,extract,feats
+import preproc.agum,preproc.rescale
 
-def single_model(frame_path):
+def single_model(frame_path,n_epochs,nn_type):
     cur_dir=os.path.split(frame_path)[0]
     nn_path,seq_path,feat_path=cur_dir+'/nn',cur_dir+'/seq',cur_dir+'/feats'
-    basic.simple_exp(frame_path,nn_path,n_epochs=150)
+    basic.simple_exp(frame_path,nn_path,n_epochs=n_epochs,model_type=nn_type)
     extract.extract_features(frame_path,nn_path,seq_path)
     feats.compute_feats(seq_path,feat_path)	
+
+def agum_seq(in_path):
+    agum_path=in_path+"_agum"
+    scale_path=in_path+"_agum_scale"
+    time_path=in_path+"_agum_time"
+    preproc.agum.agum_data(in_path,agum_path)
+    preproc.rescale.rescale(agum_path,scale_path)
+    preproc.rescale.pairs(scale_path,time_path)
