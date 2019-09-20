32e15dffbd39a05a9474728d3d59fb4f1c98ab3a
tjacek <tjacek@agh.edu.pl>
2019-09-20 12:35:14 +0200

    agum template

--- preproc/agum.py
+++ preproc/agum.py
@@ -1,16 +1,27 @@
 from imgaug import augmenters as iaa
 import imgs,data
 
+def agum_simple(raw_path,agum_path):
+    agum = iaa.Sequential([iaa.Affine(scale=(1.0, 1.5))])
+    agum_template(raw_path,agum_path,agum,n_iters=1)
+
+
 def agum_data(raw_path,agum_path):
+    crop=iaa.CropAndPad(percent=(-0.15, 0.15),sample_independently=False,keep_size=False)
+    agum =  iaa.Sequential([crop]) #iaa.SomeOf(2, [cro,crop_y])
+    agum_template(raw_path,agum_path,agum,n_iters=2)
+
+
+def agum_template(raw_path,agum_path,agum,n_iters=10):
     raw_data=imgs.read_seqs(raw_path)
     train,test=data.split(raw_data.keys())
     train_data={ name_i:raw_data[name_i] for name_i in train}
-    agum = iaa.Sequential([iaa.Affine(scale=(1.0, 1.5))])#[iaa.Crop(px=(0, 32))])
     agum_dict={}
     for name_i,seq_i in list(train_data.items()):
         agum_seq_i = agum(images=seq_i)
-        new_name_i=name_i+'_0'
-        print(new_name_i)
-        agum_dict[new_name_i]=agum_seq_i
+        for j in range(n_iters):
+            new_name_i=name_i+'_'+str(j)
+            print(new_name_i)
+            agum_dict[new_name_i]=agum_seq_i
     new_dict={**raw_data,**agum_dict}
     imgs.save_seqs(new_dict,agum_path)
