15fca578c69f388591c048328133865ad10de02b
tjacek <tjacek@agh.edu.pl>
2019-09-23 23:29:14 +0200

    diff img

--- preproc/motion.py
+++ preproc/motion.py
@@ -4,9 +4,14 @@ import imgs,files
 def action_imgs(in_path,out_path="action_imgs"):
     action_dict=imgs.read_seqs(in_path)
     files.make_dir(out_path)
+    action_dict={ name_i:diff(frames_i) for name_i,frames_i in action_dict.items()}
     for name_i,frames_i in action_dict.items():
         out_i=out_path+'/'+name_i.split(".")[0]+".png"
         cv2.imwrite(out_i,sum_imgs(frames_i))
+
+def diff(frames):
+    return[ np.abs(frames[i]-frames[i+1])
+                for i in range(len(frames)-1)]
 
 def sum_imgs(frames):
     frames=np.array(frames)
