bb1d7ce1ce43d475d7f9df74d440b6efb4a415ae
tjacek <tjacek@agh.edu.pl>
2019-10-16 22:18:24 +0200

    person features

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os.path, numpy as np
-import basic,extract,feats,ens,files,imgs
+import basic,extract,feats,ens,files,imgs,persons
 import preproc.agum,preproc.rescale,preproc.box
 
 def make_frames(in_path):
@@ -25,6 +25,13 @@ def ensemble_models(frame_path,agum_path=None,n_epochs=15,model_type='exp'):
     ens.binary_extract(frame_path,nn_path,seq_path)
     ens.binary_feats(seq_path,feat_path)
 
+def person_features(frame_path,n_epochs=100):
+    cur_dir=os.path.split(frame_path)[0]
+    model_path=cur_dir+'/person_nn'
+    feat_path=cur_dir+'/person_feats'
+    persons.person_model(frame_path,model_path,n_epochs)
+    persons.extract_person(frame_path,model_path,feat_path)
+
 def extract(frame_path,nn_path,seq_path,k=15):
     nn_paths=files.top_files(nn_path)[k:]
     print(nn_paths)
@@ -37,14 +44,6 @@ def single_model(frame_path,n_epochs,nn_type):
     extract.extract_features(frame_path,nn_path,seq_path)
     feats.compute_feats(seq_path,feat_path)	
 
-def agum_seq(in_path):
-    agum_path=in_path+"_agum"
-    scale_path=in_path+"_agum_scale"
-    time_path=in_path+"_agum_time"
-    preproc.agum.agum_data(in_path,agum_path)
-    preproc.rescale.rescale(agum_path,scale_path)
-    preproc.rescale.pairs(scale_path,time_path)
-
 def add_mode(old_path,new_path,out_path):
     old_modes=imgs.read_seqs(old_path)
     new_modes=imgs.read_seqs(new_path)
@@ -55,3 +54,11 @@ def add_mode(old_path,new_path,out_path):
         return np.concatenate([old_i,new_i],axis=1)
     unified={ name_i:add_helper(name_i) for name_i in list(new_modes.keys())}
     imgs.save_seqs(unified,out_path)
+
+#def agum_seq(in_path):
+#    agum_path=in_path+"_agum"
+#    scale_path=in_path+"_agum_scale"
+#    time_path=in_path+"_agum_time"
+#    preproc.agum.agum_data(in_path,agum_path)
+#    preproc.rescale.rescale(agum_path,scale_path)
+#    preproc.rescale.pairs(scale_path,time_path)
