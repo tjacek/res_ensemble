b6025852215757e0665a0e3a439f0939f78f2586
tjacek <tjacek@agh.edu.pl>
2019-10-20 22:42:04 +0200

    generate balanced data

--- siamese.py
+++ siamese.py
@@ -5,23 +5,24 @@ import random
 
 def train(in_path,n_size=100):
     (X_train,y_train),test,params=resnet.load_data(in_path,split=True)
-    n_samples=X_train.shape[0]
-    def rsample():
-        return random.randint(0,n_samples-1)  
-    X,y=[],[]
-    for k in range(n_size): 
-        i,j=rsample(),rsample()
-        x_i,x_j=X_train[i],X_train[j]
-        y_i,y_j=y_train[i],y_train[j]
-        x_k=[x_i,x_j]#np.concatenate([x_i,x_j],axis=1)
-        y_k=np.dot(y_i,y_j)
-        X.append(x_k)
-        y.append(y_k)
-    X,y= list(zip(*X)),keras.utils.to_categorical(y)
-    X=[np.array(X[0]), np.array(X[1]) ]
+    X,y=gen_data(X_train,y_train)
     make_models=models.ts.get_model_factory("sim")
     model=make_models(params)
     model.fit(X,y,epochs=100,batch_size=100)
+
+def gen_data(X_old,y_old):
+    n_samples,n_cats=X_old.shape[0],y_old.shape[1]
+    X,y=[],[]
+    for i in range(n_samples):
+        x_i,y_i=X_old[i],y_old[i]
+        for j in range(n_cats):
+            rn=random.randint(0,n_samples-1) 
+            x_j,y_j=X_old[rn],y_old[rn]
+            X.append([x_i,x_j])
+            y.append(np.dot(y_i,y_j))
+    X,y=np.array(X),keras.utils.to_categorical(y)
+    X=[X[:,0],X[:,1]]
+    return X,y        
 
 in_path="../MSR/sim_raw/agum"
 train(in_path,n_size=10000)
