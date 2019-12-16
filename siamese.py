d92bcf15eefe31c800aee3a2d83ac68fd2674d46
tjacek <tjacek@agh.edu.pl>
2019-12-16 20:30:30 +0100

    read seq

--- siamese.py
+++ siamese.py
@@ -1,22 +1,20 @@
 import keras
 import numpy as np
-import resnet,models.ts
 import random
 import gc
 from keras.models import load_model
 from keras.models import Model
+import resnet,models.ts
+import ens,local
 
 def extract(frame_path,model_path,out_path=None):
     extractor=load_model(model_path)
-#    feat_layer=model.get_layer('sequential_1').get_output_at(-1)
-#    extractor = Model(input=model.input,output=feat_layer)
     (X,y),names=resnet.load_data(frame_path,split=False)
     X_feats=extractor.predict(X)#[X,X])
     resnet.get_feat_dict(X_feats,names,out_path)
 
 def make_model(in_path,out_path=None,n_epochs=50):
     (X_train,y_train),test,params=resnet.load_data(in_path,split=True)
-#    raise Exception(X_train.shape)
     X,y=full_data(X_train,y_train)
     make_models=models.ts.get_model_factory("sim")
     sim_metric,model=make_models(params)
@@ -66,6 +64,14 @@ def random_data(X_old,y_old,size=100):
     X=[X[:,0],X[:,1]]
     return X,y
 
-in_path="../img/imgset"
+def preproc_data(in_path,out_path):
+    def helper(in_i,out_i):
+        seq_i=resnet.read_local_feats(in_i)
+        print(len(seq_i))
+        print(out_i)
+    ens.template(in_path,out_path,helper)
+
+in_path="../ens/binary_seq"
+preproc_data(in_path,"../sim/imgs")
 #make_model(in_path,"sim_nn",n_epochs=50)
-extract(in_path,"../img/sim_nn","../img/feats.txt")
+#extract(in_path,"../img/sim_nn","../img/feats.txt")
