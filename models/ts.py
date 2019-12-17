7fe29749ba735f493799490c5a9fca62230f9c02
tjacek <tjacek@agh.edu.pl>
2019-12-17 22:35:54 +0100

    new models

--- models/ts.py
+++ models/ts.py
@@ -12,6 +12,10 @@ from keras.models import load_model
 def get_model_factory(model_type):
     if(model_type=='sim'):
         return siamese_model
+    if(model_type=='basic_reg'):
+        return make_reg_conv
+    if(model_type=='basic_large'):
+        return make_large
     return make_conv
 
 def make_conv(params):
@@ -30,6 +34,56 @@ def make_conv(params):
     model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True),
              metrics=['accuracy'])
+    model.summary()
+    return model
+
+def make_reg_conv(params):
+    input_layer = Input(shape=(params['ts_len'], params['n_feats'],1))
+    activ='relu' #'elu'
+    pool1=add_conv_layer(input_layer,0,activ=activ,n_kerns=32)
+    
+    pool1=BatchNormalization()(pool1)
+    pool1=Dropout(0.5)(pool1)
+    pool2=add_conv_layer(pool1,1,activ=activ,kern_size=(16,1),pool_size=(8,1))
+    
+    pool2=BatchNormalization()(pool2)
+    pool2=Dropout(0.5)(pool2)
+    kernel_regularizer=regularizers.l1(0.001)
+    hidden_layer = Dense(100,name='hidden', activation=activ,
+                         kernel_regularizer=kernel_regularizer)(Flatten()(pool2))
+   
+    hidden_layer=BatchNormalization()(hidden_layer)
+    drop1=Dropout(0.5)(hidden_layer)
+    output_layer = Dense(units=params['n_cats'], activation='softmax')(drop1)
+    model=Model(inputs=input_layer, outputs=output_layer)
+    model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True),
+             metrics=['accuracy'])
+    model.summary()
+    return model
+
+
+def make_large(params):
+    input_layer = Input(shape=(params['ts_len'], params['n_feats'],1))
+    activ='relu' #'elu'
+    filters=[64,32,16]
+    layer_i=input_layer
+    for i,filtr_i in enumerate(filters):
+        layer_i=add_conv_layer(layer_i,i,activ=activ,
+                        n_kerns=filtr_i,pool_size=(2,1),kern_size=(12,1))
+        layer_i=BatchNormalization()(layer_i)
+#        layer_i=Dropout(0.5)(layer_i)
+    kernel_regularizer=regularizers.l1(0.001)
+    hidden_layer = Dense(64,name='hidden', activation=activ,
+                         kernel_regularizer=kernel_regularizer)(Flatten()(layer_i))
+   
+    hidden_layer=BatchNormalization()(hidden_layer)
+    drop1=Dropout(0.5)(hidden_layer)
+    output_layer = Dense(units=params['n_cats'], activation='softmax')(drop1)
+    model=Model(inputs=input_layer, outputs=output_layer)
+    model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.SGD(lr=0.001,  momentum=0.9, nesterov=True),
+             metrics=['accuracy'])    
     model.summary()
     return model
 
