1bce886d48c1abaa2465ae5c3a11bf3228398438
tjacek <tjacek@agh.edu.pl>
2020-01-02 22:12:41 +0100

    BalancedDist class

--- sim/dist.py
+++ sim/dist.py
@@ -1,10 +1,27 @@
+import numpy as np
+import random
 from collections import defaultdict
+
+class BalancedDist(object):
+    def __init__(self,by_cat):
+        self.by_cat=by_cat
+        self.n_cats=len(self.by_cat)
+
+    def in_cat(self,cat_i):
+        return np.random.choice(self.by_cat[cat_i])
+
+    def out_cat(self,cat_i):
+        j=random.randint(0,self.n_cats-2)
+        if(j>=cat_i):
+       	    j+=1
+        return self.in_cat(j)
 
 def make_balanced(y):
     by_cat=defaultdict(lambda :[])
     for i,y_i in enumerate(y):
         by_cat[y_i].append(i)
-    return by_cat	
+    return BalancedDist(by_cat)
 
 y=[1,1,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3]
-print(make_balanced(y))
+dist=make_balanced(y)
+print(dist.out_cat(0))
