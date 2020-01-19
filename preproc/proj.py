f5c179d6def5125e0f885a6926851c89e06f4968
tjacek <tjacek@agh.edu.pl>
2020-01-19 19:34:57 +0100

    full  projection

--- preproc/proj.py
+++ preproc/proj.py
@@ -2,17 +2,27 @@ import numpy as np
 import imgs,preproc.rescale
 
 def projection(in_path,out_path):
-    proj_x=get_helper(dim=0)
+    proj_x=get_projection(dim=0)
+    proj_y=get_projection(dim=1)
+    def full_projection(frames):
+        x_frames=proj_x(frames)
+        y_frames=proj_y(frames)
+        return [np.concatenate([proj_x,proj_y],axis=0)
+                    for proj_x,proj_y in zip(x_frames,y_frames)]
+    imgs.transform(in_path,out_path,full_projection,False)
+	
+def raw_projection(in_path,out_path,dim=0):
+    helper=get_helper(dim)
+    imgs.transform(in_path,out_path,helper,False)
+
+def get_projection(dim=0):
+    proj_x=get_helper(dim)
     def smart_proj(frames):
         x_frames=proj_x(frames)
         x_frames=preproc.rescale.smooth_proj(x_frames)
         x_frames=preproc.rescale.scale(x_frames)
         return x_frames
-    imgs.transform(in_path,out_path,smart_proj,False)
-	
-def single_projection(in_path,out_path,dim=0):
-    helper=get_helper(dim)
-    imgs.transform(in_path,out_path,helper,False)
+    return smart_proj
 
 def get_helper(dim=0):
     def proj_helper(frames):
